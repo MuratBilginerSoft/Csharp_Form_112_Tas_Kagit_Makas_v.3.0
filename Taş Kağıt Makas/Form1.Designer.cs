fb6dcfa45d3d06bd1a548bcaeb0c725431fdd998
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 23:41:31 +0300

    Initial commit

---
+++ Taş Kağıt Makas/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace Taş_Kağıt_Makas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Location = new System.Drawing.Point(14, 15);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox4.Size = new System.Drawing.Size(456, 65);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Tur Sayısı Seçim";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(18, 28);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(203, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Kaç Tur Oynamak İstiyorsunuz?";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "7",
+            "9",
+            "15",
+            "25",
+            "35",
+            "55",
+            "75",
+            "101"});
+            this.comboBox1.Location = new System.Drawing.Point(240, 26);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 24);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.groupBox5);
+            this.groupBox1.Location = new System.Drawing.Point(14, 88);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(456, 350);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oyun Alanı";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(152, 192);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(143, 145);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bilgisayarın Seçimi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 30);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.panel3);
+            this.groupBox5.Controls.Add(this.panel2);
+            this.groupBox5.Controls.Add(this.panel1);
+            this.groupBox5.Location = new System.Drawing.Point(15, 39);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox5.Size = new System.Drawing.Size(425, 126);
+            this.groupBox5.TabIndex = 7;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Seçimin";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Location = new System.Drawing.Point(294, 21);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(106, 94);
+            this.panel3.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.BackgroundImage = global::Taş_Kağıt_Makas.Properties.Resources._2;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(10, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 72);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(155, 21);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(106, 94);
+            this.panel2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImage = global::Taş_Kağıt_Makas.Properties.Resources._3;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(10, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 72);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(16, 21);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(106, 94);
+            this.panel1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImage = global::Taş_Kağıt_Makas.Properties.Resources._11;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(10, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 72);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(14, 444);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(456, 144);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sonuç";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(236, 63);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(204, 49);
+            this.label7.TabIndex = 5;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(287, 30);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 23);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "AÇIKLAMA";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(152, 63);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 54);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(21, 63);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 54);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(112, 30);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "BİLGİSAYAR";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(5, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "SEN";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(482, 602);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox4);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Taş Kağıt Makas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Taş Kağıt Makas/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Taş_Kağıt_Makas
+{
+    public partial class Form1 : Form
+    {
+        #region Tanımlamalar
+
+        Random r = new Random(); // rasgetgele sayı üretim için
+
+        int otomatik, skor1=0, skor2=0; /* otomatik pc nin rastgele seçtiği sayıyı tutacak.
+                                     * skor1 senin skorunu skor2 pc nin skorunu tutacak. */
+
+        int tur; //  oynanmak istenen  tur sayısını tutacak
+
+        int sayac = 0; // o an oynanan tur sayısını tutacak
+
+        #endregion
+
+        #region Metodlar
+
+
+        public void panelrenklendir(Panel x, Panel y, Panel z)
+        {
+            x.BackColor = Color.Yellow;
+            y.BackColor = SystemColors.Highlight;
+            z.BackColor = SystemColors.Highlight;
+        }
+
+        public void skor()
+        {
+            if (skor1 == skor2)
+                MessageBox.Show("Sen=" + skor1 + "\n" + "Bilgisayar=" + skor2 + "\n" + "Berabere kaldınız");
+            else if (skor1 > skor2)
+                MessageBox.Show("Sen=" + skor1 + "\n" + "Bilgisayar=" + skor2 + "\n" + "Kazandın mübarek olsun.");
+            else if (skor1 < skor2)
+                MessageBox.Show("Sen=" + skor1 + "\n" + "Bilgisayar=" + skor2 + "\n" + "Olmadı hacı bir dahaki sefere artık.");
+        
+        }
+
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // oyun ilk açıldığında;
+
+            /* oyunu ilk çalıştığında tur seçimi yapılana kadar ekranda oyuna ait
+             * hiç bir nesne olmasını istemiyoruz ve bu yüzden grouboxların visible 
+             * yani görünürlük özelliklerini false yaptık */
+
+            #region groubbox görünmez
+
+            groupBox1.Visible = false; 
+            groupBox2.Visible = false;
+            groupBox3.Visible = false;
+
+            #endregion
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            /* comboboxta seçim yapıldığı anda yapılacak işlemler */
+
+            tur = int.Parse(comboBox1.SelectedItem.ToString()); // seçilen değeri tur değişkenine atadık.  
+
+            groupBox4.Visible = false;
+
+            #region grroubboxgörünür
+
+            // oyun nesnelerini görünür hale getirdik.
+
+            groupBox1.Visible = true;
+            groupBox2.Visible = true;
+            groupBox3.Visible = true;
+
+            #endregion
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // taş butonuna tıklandığında
+
+            #region if 1
+
+            if (sayac < tur) /* ilk başta sayacı 0 dan başlattığım için küçük dedim
+                              * sayacı 1 den başlatsa idim <= yapmam gerekirdi. */
+            {
+                #region panelrengi
+
+                //tıklanan butonun arkaplandaki panelini siyah yaparak seçimimizi belirgin hale getirdik.
+
+                panelrenklendir(panel1, panel2, panel3);
+               
+                #endregion
+
+                sayac++; // oynan tur sayısını tutan sayacı bir artırdık.
+
+                otomatik = r.Next(3); // 3 tane seçenek olduğu için 0 ile 3 arasında rastgele değer aldık.
+
+                #region if 2
+                if (otomatik == 0) // bilgisayarın seçtiği sayı 0 ise bu taş nesnesine denk geliyor.
+                {
+                    pictureBox1.Image = button1.BackgroundImage;
+                    label7.Text = "İki seçimde aynı";
+                }
+
+                else if (otomatik == 1) // bilgisayarın seçtiği sayı 1 ise bu  kağıt nesnesine denk geliyor.
+                {
+                    pictureBox1.Image = button2.BackgroundImage;
+                    label7.Text = "Kağıt taşı alır :(";
+                    skor2++;
+                    label5.Text = skor2.ToString();
+
+
+                }
+
+                else if (otomatik == 2) // bilgisayarın seçtiği sayı 2 ise bu makas nesnesine denk geliyor.
+                {
+                    pictureBox1.Image = button3.BackgroundImage;
+                    label7.Text = "Taş Makası Kırar";
+                    skor1++;
+                    label4.Text = skor1.ToString();
+
+                }
+
+                #endregion
+            }
+            #endregion
+
+            
+
+            else /* sayaç tur sayısından büyük olursa çalışacak kodlar.
+                  * karşılaştırmaları yapıp oyun sonucunu bir messagebox ile
+                  * ekranda gösteren kodlar. */
+            {
+                skor();
+            }
+
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            // kağıt tuşuna tıklandığında
+
+            #region if 1
+
+            if (sayac < tur)
+            {
+                panelrenklendir(panel2, panel1, panel3);
+
+                sayac++;
+                otomatik = r.Next(3); // 3 tane seçenek olduğu için 0 ile 3 arasında rastgele değer aldık.
+
+                #region if 2
+                if (otomatik == 0)
+                {
+                    pictureBox1.Image = button1.BackgroundImage;
+                    label7.Text = "Kağıt Taşı sarar";
+                    skor1++;
+                    label4.Text = skor1.ToString();
+                }
+
+                else if (otomatik == 1)
+                {
+                    pictureBox1.Image = button2.BackgroundImage;
+                    label7.Text = "iki seçimde aynı";
+                }
+
+                else if (otomatik == 2)
+                {
+                    pictureBox1.Image = button3.BackgroundImage;
+                    label7.Text = "Makas Kağıdı Keser";
+                    skor2++;
+                    label5.Text = skor2.ToString();
+
+                }
+
+                #endregion
+            }
+            #endregion
+ 
+            else
+            {
+                skor();
+            }
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            // makas tuşuna tıklandığında
+
+            #region if 1
+            if (sayac < tur)
+            {
+                panelrenklendir(panel3, panel2, panel1);
+                sayac++;
+                otomatik = r.Next(3); // 3 tane seçenek olduğu için 0 ile 3 arasında rastgele değer aldık.
+
+                #region if 2
+
+                if (otomatik == 0)
+                {
+                    pictureBox1.Image = button1.BackgroundImage;
+                    label7.Text = "Taş Makasın Kırar";
+                    skor2++;
+                    label5.Text = skor2.ToString();
+                }
+
+                else if (otomatik == 1)
+                {
+                    pictureBox1.Image = button2.BackgroundImage;
+                    label7.Text = "Makas Kağıdı Keser";
+                    skor1++;
+                    label4.Text = skor1.ToString();
+                }
+
+                else if (otomatik == 2)
+                {
+                    pictureBox1.Image = button3.BackgroundImage;
+                    label7.Text = "İki seçimde aynı";
+                }
+
+                #endregion
+            }
+            #endregion
+
+            else
+            {
+                skor();
+            }
+            
+        }
+    }
+}
